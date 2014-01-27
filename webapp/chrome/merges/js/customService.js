58e5df20cc4628bde0aedf855fae30ad6a11ed9c
Drake Emko <demko@revionics.com>
2014-01-27 00:49:33 -0700

    fixing up app for chrome store and cloudfront / s3 distribution

--- webapp/chrome/merges/js/customService.js
+++ webapp/chrome/merges/js/customService.js
@@ -6,17 +6,17 @@ angular.module('app.customService', []).
 				window.open(encodeURI(link)); 
 			},
 
-			trackPage: function(page) {/*
+			trackPage: function(page) {
 				$timeout(function() {
-				  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-				  ga('create', 'UA-46128370-1', 'd26tzgk0gx1hwh.cloudfront.net');
-				  ga('send', 'pageview', {
-					'page': page
-				  });
-				}, 1500);*/
+					(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+					})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+					ga('create', 'UA-47455659-2', 'd2y538ab9a8vah.cloudfront.net');
+					ga('send', 'pageview', {
+						'page': page
+					});
+				}, 1500);
 			},
 
 			doCustomActions: function() {
