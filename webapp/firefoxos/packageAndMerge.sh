637f2e7fe7dbafa43c1aecec59f3df16d20594ad
Drake Emko <demko@revionics.com>
2014-01-26 21:16:49 -0700

    fixed firefoxos scripts

--- webapp/firefoxos/packageAndMerge.sh
+++ webapp/firefoxos/packageAndMerge.sh
@@ -2,7 +2,7 @@
 rm -fr www/*
 
 # copy web assets from main project's www directory
-cp -a ../www/ www/
+cp -a ../../www/ www/
 
 # overwrite default web assets with any firefoxos-specific versions of those files
 cp -a merges/ www/
