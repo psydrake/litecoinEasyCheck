831521b7b985739c336c0222df752072652ef733
Drake Emko <demko@revionics.com>
2014-01-27 19:12:42 -0700

    cached more front page stats. added link to Litecoin news in footer

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,7 +12,9 @@ angular.module('app.controllers', []).
         $scope.price_before_24h = settingsService.getNumValue('price_before_24h'); // last traded price 24 hours before
         $scope.price = settingsService.getNumValue('price'); // last traded price of best market
         $scope.best_market = settingsService.getBestMarket(); // market with the most volume for this trading pair
-        $scope.volume = settingsService.getNumValue('volume'); // trading volume expressed as LTC
+        $scope.volume = settingsService.getNumValue('volume'); // trading volume expressed as the selected currency
+        $scope.volume_ltc = settingsService.getNumValue('volume_ltc'); // trading volume expressed as LTC
+		$scope.price_btc = settingsService.getNumValue('price_btc'); // price of LTC in BTC
 
         $scope.loadData = function() {
             cryptocoinchartsAPIService.getLTCTrading('BTC').success(function (response) {
@@ -48,9 +50,6 @@ angular.module('app.controllers', []).
 
                     settingsService.setNumValue('volume_ltc', response.volume_first);
                     $scope.volume_ltc = settingsService.getNumValue('volume_ltc');
-
-                    settingsService.setNumValue('volume_btc', response.volume_btc);
-                    $scope.volume_btc = settingsService.getNumValue('volume_btc');
 				}
 				else {
 					$log.warn('Warning: No trading data returned from cryptocoinchartsAPIService.getLTCTrading(' + $scope.currency + ')', response);
