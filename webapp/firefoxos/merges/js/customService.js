a7995d765d242ce527034467f0920264654890f0
Drake Emko <demko@revionics.com>
2014-01-29 02:52:22 -0700

    used google analytics code for firefox os hosted app

--- webapp/firefoxos/merges/js/customService.js
+++ webapp/firefoxos/merges/js/customService.js
@@ -7,18 +7,16 @@ angular.module('app.customService', []).
 			},
 
 			trackPage: function(page) {
-			/*
 				$timeout(function() {
-				  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-				  ga('create', 'UA-46128370-3', 'd23qh8rj19dgra.cloudfront.net');
-				  ga('send', 'pageview', {
-					'page': page
-				  });
+					(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+						(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+						 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+						})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+					ga('create', 'UA-47455659-3', 'd49etxmu12kf2.cloudfront.net');
+					ga('send', 'pageview', {
+						'page': page
+					});
 				}, 1500);
-			*/
 			},
 
 			doCustomActions: function() {
