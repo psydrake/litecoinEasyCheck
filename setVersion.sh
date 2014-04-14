75646a008321e0c51293c24cae06f7a824887106
Drake Emko <demko@revionics.com>
2014-04-14 15:43:56 -0600

    incremented version code and number

--- setVersion.sh
+++ setVersion.sh
@@ -6,10 +6,10 @@
 # 2. run this script: ./setVersion.sh
 
 # Universal version - in Android, this is versionName
-VERSION=1.1
+VERSION=1.2
 
 # Only used in Android. Appended as minor version number if avaliable
-VERSION_CODE=4
+VERSION_CODE=5
 
 perl -pi -e "s/\sversion=\"\d+\.\d+\"\s/\ version=\"${VERSION}\"\ /" www/config.xml
 perl -pi -e "s/return\s\'\d+\.\d+\.\d+\'\;/return\ \'${VERSION}\.${VERSION_CODE}\'\;/" www/js/services.js 
